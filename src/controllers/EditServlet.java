41012f85b1be1e5df639160d997f1847cfe1b182
kenta.matsuo <superace609@gmail.com>
2022-05-23 17:37:58 +0900

    存在しないidに対する処理を追加

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -3,7 +3,6 @@ package controllers;
 import java.io.IOException;
 
 import javax.persistence.EntityManager;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -33,19 +32,19 @@ public class EditServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
-        // 該当のIDのメッセージ1件のみをデータベースから取得
+        // 該当のIDのタスク1件のみをデータベースから取得
         Task m = em.find(Task.class, Integer.parseInt(request.getParameter("id")));
 
         em.close();
 
-        // メッセージ情報とセッションIDをリクエストスコープに登録
+        // タスク情報とセッションIDをリクエストスコープに登録
         request.setAttribute("task", m);
         request.setAttribute("_token", request.getSession().getId());
 
-        // メッセージIDをセッションスコープに登録
-        request.getSession().setAttribute("message_id", m.getId());
-
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/edit.jsp");
-        rd.forward(request, response);
+        // タスクデータが存在しているときのみ
+        // タスクIDをセッションスコープに登録
+        if(m != null) {
+            request.getSession().setAttribute("task_id", m.getId());
+        }
     }
 }
